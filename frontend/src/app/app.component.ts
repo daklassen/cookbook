7d2cd273542f3fac03ad6ca9eb63a45d4940863c
David Klassen <daviddd.kl@gmail.com>
2018-07-24 21:25:23 +0200

    Improve design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/takeWhile';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(translate: TranslateService) {
