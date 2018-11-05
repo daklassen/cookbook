19a0c6d3ed56ec446cde2365c6fa0177d3acdf5e
David Klassen <daviddd.kl@gmail.com>
2018-11-05 16:28:02 +0100

    Exclude sample data creation from application class

--- backend/src/main/java/de/david/cookbook/CookBookApplication.java
+++ backend/src/main/java/de/david/cookbook/CookBookApplication.java
@@ -1,55 +1,22 @@
 package de.david.cookbook;
 
-import de.david.cookbook.persistence.entities.Category;
-import de.david.cookbook.persistence.entities.Ingredient;
-import de.david.cookbook.persistence.entities.Recipe;
-import de.david.cookbook.persistence.entities.User;
-import de.david.cookbook.persistence.repositories.CategoryRepository;
-import de.david.cookbook.persistence.repositories.IngredientRepository;
-import de.david.cookbook.persistence.repositories.RecipeRepository;
-import de.david.cookbook.persistence.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class CookBookApplication implements CommandLineRunner {
 
     @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    private RecipeRepository recipeRepository;
-
-    @Autowired
-    private IngredientRepository ingredientRepository;
-
-    @Autowired
-    private UserRepository userRepository;
+    private SampleData sampleData;
 
     public static void main(String[] args) {
         SpringApplication.run(CookBookApplication.class, args);
     }
 
-    // TODO: remove temporary content filling on startup
     @Override
     public void run(String... args) throws Exception {
-        Category hauptspeise = new Category("Hauptspeise");
-        Category vorspeise = new Category("Vorspeise");
-        Category nachspeise = new Category("Nachspeise");
-        categoryRepository.save(hauptspeise);
-        categoryRepository.save(vorspeise);
-        categoryRepository.save(nachspeise);
-
-        User david = new User("David", "Klassen", "david@online.de", "7fb96b6b-175b-4f06-bf76-64b451b27086");
-        userRepository.save(david);
-
-        Category testcategory = categoryRepository.save(new Category("Testcategory"));
-        Ingredient zucker = ingredientRepository.save(new Ingredient(1, "Zucker", "Kg"));
-        Recipe recipe = new Recipe("Testrecipe 1", david, "https://image.ibb.co/hpQUy7/food_3.png", 4, "description", testcategory, Arrays.asList(zucker));
-        recipeRepository.save(recipe);
+        sampleData.createSampleData();
     }
 }
