637be08d72b2e582c87c9f67d801b19edd5ac358
David Klassen <daviddd.kl@gmail.com>
2018-11-29 16:40:23 +0100

    Introduce material dialogs

--- frontend/src/app/recipe/components/recipe-edit/recipe-edit.component.ts
+++ frontend/src/app/recipe/components/recipe-edit/recipe-edit.component.ts
@@ -7,7 +7,8 @@ import { Breadcrumb } from 'src/app/shared/models/Breadcrumb';
 import { RecipeDTO } from 'src/app/shared/services/recipe/transfer/RecipeDTO';
 import { RecipeService } from 'src/app/shared/services/recipe/recipe.service';
 import { SnackbarService } from 'src/app/shared/services/ui/snackbar.service';
-import { finalize, takeWhile } from 'rxjs/operators';
+import { finalize, takeWhile, take } from 'rxjs/operators';
+import { DialogService } from 'src/app/shared/services/ui/dialog.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -27,7 +28,8 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private router: Router,
     private spinner: NgxSpinnerService,
-    private snackBar: SnackbarService
+    private snackBar: SnackbarService,
+    private dialogService: DialogService
   ) {}
 
   public ngOnInit(): void {
@@ -61,24 +63,15 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
       );
   }
 
-  public onDeleteRecipe(): void {
-    this.spinner.show();
-    this.recipeService
-      .deleteRecipe(this.recipe.id)
-      .pipe(
-        finalize(() => this.spinner.hide()),
-        takeWhile(() => this.viewAlive)
-      )
-      .subscribe(
-        success => {
-          this.snackBar.openShortSnackbar('GENERAL.DELETE_SUCCESS');
-          this.router.navigateByUrl('/recipes');
-        },
-        error => {
-          this.snackBar.openShortSnackbar('GENERAL.DELETE_ERROR');
-          console.error(error);
+  public onDeleteRecipeRequest(): void {
+    this.dialogService
+      .openConfirmDialog('EDIT_RECIPE_PAGE.DELETE_CONFIRM')
+      .pipe(take(1))
+      .subscribe((confirmed: boolean) => {
+        if (confirmed) {
+          this.deleteRecipe();
         }
-      );
+      });
   }
 
   public onAbortClicked(recipe: RecipeDTO): void {
@@ -99,6 +92,26 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
           this.generateBreadcrumbs();
         },
         error => console.error(error)
+      );
+  }
+
+  private deleteRecipe(): void {
+    this.spinner.show();
+    this.recipeService
+      .deleteRecipe(this.recipe.id)
+      .pipe(
+        finalize(() => this.spinner.hide()),
+        takeWhile(() => this.viewAlive)
+      )
+      .subscribe(
+        success => {
+          this.snackBar.openShortSnackbar('GENERAL.DELETE_SUCCESS');
+          this.router.navigateByUrl('/recipes');
+        },
+        error => {
+          this.snackBar.openShortSnackbar('GENERAL.DELETE_ERROR');
+          console.error(error);
+        }
       );
   }
 
