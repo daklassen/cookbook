7c7a4ee226f189ed70662f9d7d7180e07df10c4a
David Klassen <daviddd.kl@gmail.com>
2019-01-27 20:06:05 +0100

    Repair pipeline

---
+++ src/app/shell/components/footer/footer..component.spec.ts
@@ -0,0 +1,23 @@
+import { FooterComponent } from './footer.component';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [FooterComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
